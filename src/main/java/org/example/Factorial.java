867ff6bfc0f356becd32f28f89c5a2f8aacc7e5f
Oleg Babenko <o.babenko236@gmail.com>
2024-10-16 16:36:11 +0800

    A factorial calculation program has been created

---
+++ src/main/java/org/example/Factorial.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.math.BigInteger;
+
+public class Factorial {
+    public static void getFactorial(int f) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= f; i++)
+            result = result.multiply(BigInteger.valueOf(i));
+        System.out.println(result);
+    }
+}
