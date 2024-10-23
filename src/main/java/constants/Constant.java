66a20f774a3c9c332b74bea98199e81a64e3ca59
Oleg Babenko <o.babenko236@gmail.com>
2024-10-24 03:05:58 +0800

    Lesson_15 is complete.

---
+++ src/main/java/constants/Constant.java
@@ -0,0 +1,13 @@
+package constants;
+
+public class Constant {
+    public static class TimeoutVariable {
+        public static final int IMPLICIT_VALUE = 5;
+        public static final String TEST_NUMBER = "297777777";
+        public static final String TEST_SUM = "25";
+    }
+
+    public static class Urls {
+        public static final String MTS_HOME_PAGE = "https://www.mts.by/";
+    }
+}
